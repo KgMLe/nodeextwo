c5457a5595f4402877f19440dbe2f3c5f8427387
KgMLe <kgodisholebopa@gmail.com>
2023-07-31 15:53:04 +0200

    exercisetwo

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const Person = require('./people');
+
+// Create three different Person objects
+const personone = new Person('Kgodisho', 'Lebopa', 'kgodisholebopa@gmail.com');
+const persontwo = new Person('Addy', 'Mojapelo', 'addz@yahoo.com');
+const personthree = new Person('Kapodi', 'Jalebo', 'japodi.k@outlook.com');
+
+// Now you can work with these person objects
+console.log(personone);
+console.log(persontwo);
+console.log(personthree);
---
+++ people.js
@@ -0,0 +1,8 @@
+const Person = (firstName, lastName, emailAddress) => {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  };
+  
+  module.exports = Person;
+  
